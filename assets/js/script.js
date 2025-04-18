f182792bb5643d41e069ee72a698efddadfec528
hb-ministories <disenoiake@gmail.com>
2025-04-18 13:15:58 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-// JS opcional
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".flipbook");
+  const pages = document.querySelectorAll(".pagina");
+  const totalPages = pages.length;
+  let currentPage = 0;
+  let startX = 0;
+
+  container.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+  });
+
+  container.addEventListener("touchend", (e) => {
+    const endX = e.changedTouches[0].clientX;
+    const delta = startX - endX;
+
+    if (delta > 50 && currentPage < totalPages - 1) {
+      currentPage++;
+    } else if (delta < -50 && currentPage > 0) {
+      currentPage--;
+    }
+
+    pages[currentPage].scrollIntoView({ behavior: "smooth" });
+  });
+});
